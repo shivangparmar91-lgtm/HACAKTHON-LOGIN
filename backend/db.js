ed39c1fab8a11387b69ac0b60f8d0c59ec96cb38
Shivang Parmar <shivangparmar91@gmail.com>
2026-05-09 14:33:04 +0530

    Add database port

--- backend/db.js
+++ backend/db.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 // Create the connection pool to the database
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // <-- ADD THIS LINE!
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-
   ssl: { rejectUnauthorized: false }
 });
 
